c8fa278aad2d976c9082793dc335dc6532c7a046
ehan95266 <ehan0526@gmail.com>
2023-02-21 19:38:18 -0500

    HRMWebApi

---
+++ HrmApi/HumanResource.APILayer/Controller/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HumanResource.APILayer.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("This is working successfully");
+        }
+
+        [HttpGet]
+        [Route("{name}")]
+        public IActionResult Get(string name)
+        {
+            return Ok(new { Id = 1, Name= name, Age=30, City="SpringField"});
+        }
+
+        // GET: api/values
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
+
---
+++ HrmApi/HumanResource.APILayer/Program.cs
@@ -0,0 +1,44 @@
+﻿using Hrm.ApplicationCore.Contract.Repository;
+using Hrm.ApplicationCore.Contract.Service;
+using Hrm.Infrastructure.Data;
+using Hrm.Infrastructure.Repository;
+using Hrm.Infrastructure.Service;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+//dependency injection for connectionstring
+builder.Services.AddDbContext<HrmDbContext>(option => {
+    option.UseSqlServer(builder.Configuration.GetConnectionString("HrmApiDb"));
+
+});
+
+//dependency injection for repository
+builder.Services.AddScoped<ICandidateRepositoryAsync, CandidateRepositoryAsync>();
+
+//dependency injection for services
+builder.Services.AddScoped<ICandidateServiceAsync, CandidateServiceAsync>();
+
+var app = builder.Build();
+app.UseRouting();//middleware allows to use routing
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+
+app.Run();
+
+
